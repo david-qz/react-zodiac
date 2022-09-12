0b8a5742b5664a89beac16820bd179b6448e7317
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-12 14:44:18 -0700

    Add a header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <Main />
   </React.StrictMode>
 );
