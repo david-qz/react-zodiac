5438d3bfeb610d08032765a624e7e00c4e624dca
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-12 15:57:18 -0700

    Initial ZodiacCard styling and layout

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,14 @@
 import './ZodiacCard.css';
 
-export default function ZodiacCard({ zodiacSign }) {
-  return <p>{zodiacSign.name}</p>;
+export default function ZodiacCard({ zodiacSign: { name, symbol, dates } }) {
+  name = name[0].toUpperCase() + name.slice(1);
+
+  return (
+    <div className='ZodiacCard'>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/signs/${name}.png`}/>
+      <span className='name'>{name}</span>
+      <span className='symbol'>{symbol}</span>
+      <span className='dates'>{dates}</span>
+    </div>
+  );
 }
