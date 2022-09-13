237f1c97ceefd4b7afefd4cb48b66d3364fff3a9
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-12 17:59:44 -0700

    Appease linter

--- functions/cat-endpoint.js
+++ functions/cat-endpoint.js
@@ -8,18 +8,20 @@ const headers = {
 };
 
 
+// eslint-disable-next-line no-unused-vars
 exports.handler = async (event, context) => {
   try {
     const response = await fetch('https://cat-fact.herokuapp.com/facts');
     const data = await response.json();
     const json = JSON.stringify(data);
-    
-    return { 
-      statusCode: 200, 
+
+    return {
+      statusCode: 200,
       headers,
       body: json
     };
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log(error);
     return {
       statusCode: 500,
