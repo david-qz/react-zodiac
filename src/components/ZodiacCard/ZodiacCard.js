e3c5a86626fa5686e654c76d2eefd6cb904a03aa
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-12 17:22:30 -0700

    Import a background image
    It's kind of silly to move this one style declaration in line because
    the bundler will handle local paths in the CSS, but it's a rubric item.

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,10 +1,14 @@
 import './ZodiacCard.css';
+import background from '../../images/parchment.jpg';
 
 export default function ZodiacCard({ zodiacSign: { name, symbol, dates } }) {
   name = name[0].toUpperCase() + name.slice(1);
 
   return (
-    <div className='ZodiacCard'>
+    <div
+      className='ZodiacCard'
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <img alt={name} src={`${process.env.PUBLIC_URL}/signs/${name}.png`}/>
       <span className='name'>{name}</span>
       <span className='symbol'>{symbol}</span>
