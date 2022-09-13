3d6677ace066f63022c6740af3a7f3211f08e597
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-12 22:34:37 -0700

    Add random transformations to cards
    As long as I have the felt background, I might as well try to lean into
    the skeuomorphic thing.

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,13 +1,30 @@
 import './ZodiacCard.css';
 import background from '../../images/parchment.jpg';
+import { useState } from 'react';
+
+const ROTATION_SPREAD = 2;
+const TRANSLATION_SPREAD = 20;
+
+function randomTransformation() {
+  const rotation = (Math.random() - 0.5) * ROTATION_SPREAD;
+  const offsetX = (Math.random() - 0.5) * TRANSLATION_SPREAD;
+  const offsetY = (Math.random() - 0.5) * TRANSLATION_SPREAD;
+
+  return `rotate(${rotation}deg) translate(${offsetX}px, ${offsetY}px)`;
+}
 
 export default function ZodiacCard({ zodiacSign: { name, symbol, dates } }) {
-  name = name[0].toUpperCase() + name.slice(1);
+  const [transformation] = useState(() => randomTransformation());
 
+  name = name[0].toUpperCase() + name.slice(1);
   return (
     <div
       className='ZodiacCard'
-      style={{ backgroundImage: `url(${background})` }}
+      style={{
+        backgroundImage: `url(${background})`,
+        transform: transformation,
+        zIndex: -1
+      }}
     >
       <span className='name'>{name}</span>
       <span className='symbol'>The {symbol}</span>
